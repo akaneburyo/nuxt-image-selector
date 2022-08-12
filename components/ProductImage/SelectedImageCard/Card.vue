3358c24dc261108e05177698237c08f144ecaf30
akaneburyo <akaneburyo@gmail.com>
2022-08-12 14:40:14 +0900

    added components, fixed page layout.

---
+++ components/ProductImage/SelectedImageCard/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <CAspectRatioBox max-w="160px" :ratio="1">
+    <CImage
+      :src="imageUrl"
+      w="100%"
+      h="100%"
+      object-fit="contain"
+      border="1px"
+      borderColor="gray.200"
+    />
+  </CAspectRatioBox>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { CImage, CAspectRatioBox } from '@chakra-ui/vue'
+
+export default Vue.extend({
+  components: {
+    CImage,
+    CAspectRatioBox,
+  },
+  props: {
+    imageUrl: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+})
+</script>
